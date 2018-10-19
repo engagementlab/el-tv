6aa474973058c1eab8bc72071a53a98f7e8d5c9f
Johnny Richardson <johnny.c.rich@gmail.com>
2018-10-19 14:53:06 -0400

    Overhauled slideshow w/ basic express app.

--- mean/routes/index.js
+++ mean/routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+'use strict'
+
+const express = require('express'),
+			router = express.Router(),
+			request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+	let url = (process.env.NODE_ENV === 'production') ? 'https://elab.emerson.edu/' : 'https://qa.engagementgamelab.org/';
+	request({ url: url+'api/tv/get', json: true }, (error, response, body) => {
+
+		let data = {
+			images: body[0].slideshowImages,
+			chyron: body[0].currentBlurb
+		};
+	  res.render('index', { 'data': data });
+
+	});
+
 });
 
 module.exports = router;
