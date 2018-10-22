ad3858082299f6ae65f728ec8265428f02e93ea3
Johnny Richardson <johnny.c.rich@gmail.com>
2018-10-22 11:57:52 -0400

    Allow for video display, adjust chyron padding.

--- config/config.js
+++
@@ -1,6 +0,0 @@
-// Copy this file as config.js in the same folder, with the proper database connection URI.
-
-module.exports = {
-  db: 'mongodb://127.0.0.1:27017/el-tv',
-  db_dev: 'mongodb://127.0.0.1:27017/el-tv',
-};
