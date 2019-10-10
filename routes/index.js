292a6756c8f19cb173e403bdaccc94d202d573d5
Johnny Richardson <johnny.c.rich@gmail.com>
2019-10-10 17:22:59 -0400

    WIP transition to slideshow in embed video.

--- routes/index.js
+++ routes/index.js
@@ -8,18 +8,19 @@ const express = require('express'),
 router.get('/', function(req, res, next) {
 
 	let url = (process.env.NODE_ENV === 'production') ? 'https://elab.emerson.edu/' : 'https://qa.engagementgamelab.org/';
-	request({ url: url+'api/tv/get', json: true }, (error, response, body) => {
+	let data = 'https://res.cloudinary.com/engagement-lab-home/video/upload/v1570742177/el-tv/slideshow.mp4';
+	res.render('index', { 'data': data });
+	// request({ url: url+'api/tv/get', json: true }, (error, response, body) => {
 
-		let data = {
-			images: body[0].slideshowImages,
-			chyron: JSON.stringify(body[0].currentBlurb),
-			hasVideo: body[0].displayVideo,
-			videoId:  body[0].videoId
-		};
+	// 	let data = {
+	// 		images: body[0].slideshowImages,
+	// 		chyron: JSON.stringify(body[0].currentBlurb),
+	// 		hasVideo: body[0].displayVideo,
+	// 		videoId:  body[0].videoId
+	// 	};
 		
-	  res.render('index', { 'data': data });
 
-	});
+	// });
 
 });
 
